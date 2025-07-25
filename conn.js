22c0f5ce671820d314c3684bde2d27490e283535
Ayush Rai <145470522+ayush123-bit@users.noreply.github.com>
2025-07-26 00:51:46 +0530

    Update conn.js

--- conn.js
+++ conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://userayush:1234@cluster0.p7zfp.mongodb.net/MallDataManage")
+mongoose.connect("Your Mongo Uri")
   .then(() => {
     console.log("Connection Successful");
   })
